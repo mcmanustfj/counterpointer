3963200d6d0967b1a67a98923b16c4280799483a
Thomas McManus <mcmanustfj@gmail.com>
2017-09-28 15:25:57 -0400

    begun generation

--- Counterpointer.py
+++ Counterpointer.py
@@ -1,15 +1,37 @@
+import music21
 from music21 import *
 from music21.interval import Interval
 from music21.stream import Stream
 
+allowedVerticalIntervals = {"m3", "M3", "P5", "m6", "M6", "P8"}
+allowedHorizontalIntervals = {"P1", "m2", "M2", "m3", "M3", "P4", "P5", "m6", "M6", "P8"}
+
+
+#generate species 1 counterpoint from cantus firmus
+#only works with no accidentals currently
+def generateMelody(cf: stream.Stream):
+    cflist = list(cf.flat.notes)
+    posslist = []
+    for note in cflist:
+        newlist = []
+        for intstr in allowedVerticalIntervals:
+            next = note.transpose(Interval(intstr))
+            #only attach pitches in the mode
+            pitches =  music21.scale.Scale.extractPitchList(music21.key.Key("C", "major").getScale())
+            if next.pitch.pitchClass in [p.pitchClass for p in pitches]:
+                newlist.append(next)
+        posslist.append(newlist)
+
+    return posslist
+
+
+
 
 #verify species 1 counterpoint
-#melody1 is a music21.stream.Stream of the cantus firumus, melody2 is the secondary melody
+#melody1 is a music21.stream.Stream of the cantus firmus, melody2 is the secondary melody
 def verifyCounterpointVerbose(melody1: stream.Stream, melody2: stream.Stream) -> bool:
 
     rval = True
-    allowedVerticalIntervals = {"P1", "m3", "M3", "P5", "m6", "M6", "P8"}
-    allowedHorizontalIntervals = {"P1","m2", "M2", "m3", "M3", "P4", "P5", "m6", "M6", "P8"}
 
     melody1 = melody1.flat.notes
     melody2 = melody2.flat.notes
@@ -40,7 +62,6 @@ def verifyCounterpointVerbose(melody1: stream.Stream, melody2: stream.Stream) ->
         if i != 0 and Interval(melody2[i-1], melody2[i]).name not in allowedHorizontalIntervals:
             badHorIntM2.append((i-1, i, Interval(melody2[i-1], melody2[i])))
 
-
     if not len(badVerticalIntervalList) == 0:
         print("Bad vertical intervals:")
     for v in badVerticalIntervalList:
@@ -63,5 +84,12 @@ def testVerify():
 
     verifyCounterpointVerbose(s1, s2)
 
+
+def testGen():
+    s1 = Stream(converter.parse("tinyNotation: d1 a g f e d f e d'"))
+    s2 = generateMelody(s1)
+    for slist in s2:
+        print(slist)
+
 if __name__ == "__main__":
-    testVerify()
+    testGen()
